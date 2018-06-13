4c0697c8b9f51335dc298b50174321b0c0949ba8
caoyu <caoyu@ads-media.cn>
2018-06-13 18:26:58 +0800

    -

---
+++ frame/tp5/public/static/common/js/common.js
@@ -0,0 +1,21 @@
+/**
+ * 检测是pc 还是 移动
+ *
+ * @return {bool} [true pc false orther]
+ */
+function IsPC()
+{  
+	var userAgentInfo = navigator.userAgent;  
+	var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod");  
+	var flag = true;
+	for (var v = 0; v < Agents.length; v++) 
+	{
+	   if (userAgentInfo.indexOf(Agents[v]) > 0) 
+	   { 
+	   		flag = false;
+	   		break; 
+	   }  
+	}
+
+	return flag;  
+}
