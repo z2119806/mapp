56ac0534e8bb281236324000d315dd9b96b1de5a
caoyu <caoyu@ads-media.cn>
2019-04-28 18:34:12 +0800

    no message

--- py/test/first/1.py
+++ py/test/first/1.py
@@ -22,6 +22,19 @@ def sigmoid(x):
 # plt.legend()
 # plt.show()
 
-A = np.array([[1, 2], [3, 4]])
-B = np.array([[5, 6], [7, 8]])
-print(np.dot(A, B))
+# 输入层到第一层神经元
+X = np.array([1.0, 0.5])
+W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+B1 = np.array([0.1, 0.2, 0.3])
+A1 = np.dot(X, W1) + B1
+Z1 = sigmoid(A1) # 激活函数激活
+
+# 第一层到第二层
+
+W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+B2 = np.array([0.1, 0.2])
+
+A2 = np.dot(Z1, W2) + B2
+Z2 = sigmoid(A2)
+
+print(Z2)
