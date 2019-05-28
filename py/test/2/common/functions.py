e172ee260aa0018ac6e5fb78b7dbd272b148ca32
caoyu <caoyu@ads-media.cn>
2019-05-28 18:05:25 +0800

    -

--- py/test/2/common/functions.py
+++ py/test/2/common/functions.py
@@ -1,6 +1,13 @@
 import numpy as np
 
-# 激活函数
+"""
+激活函数-sigmoid函数
+主要用于求曲线 
+-1/大于1取值范围1-0之间 
+?-x为倒序 x升序
+为什么用0和1，概率问题0%-100%
+形参值越大，返回越大，反之..
+"""
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
@@ -9,12 +16,17 @@ def sigmoid(x):
 def identity_function(x):
     return x
 
-# softmax函数
+"""
+softmax函数
+输出函数
+作用：不会对输入元素的大小位置所改变，将各个值转换成总和为1的小数，以便求概率。
+也就是每个输入元素 / 所有元素的总和，引入e是避免循环
+"""
 def softmax(x):
     c = np.max(x)
-    exp_a = np.exp(x - c) # 防止溢出，减去相同数，结果不变
+    exp_a = np.exp(x - c) # 防止溢出，减去元素最大值，结果不变
     sum_exp_a = np.sum(exp_a)
-    y = exp_a / sum_exp_a
+    y = exp_a / sum_exp_a：
 
     return y
 # def softmax(x):
---
+++ py/test/2/test2.py
@@ -0,0 +1,86 @@
+# coding: utf-8
+import sys, os
+sys.path.append(os.pardir)  # 为了导入父目录的文件而进行的设定
+import numpy as np
+import pickle # pkl文件
+from dataset.mnist import load_mnist # 数据
+from common.functions import sigmoid, softmax # 函数
+#from PIL import Image
+
+def get_data():
+    # 训练数据，训练标签；测试数据，测试标签，normaize=True预处理 / 255
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
+    return x_test, t_test
+
+# 去掉预处理
+# def apeal_data(x):
+#     return x * 255
+
+def init_network():
+    with open("other/sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
+    return network
+
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = softmax(a3)
+
+    return y
+
+x, t = get_data()
+network = init_network()
+
+batch_size = 100
+accuracy_cnt = 0
+
+# def img_show(img):
+#     pil_img = Image.fromarray(np.uint8(img))
+#     pil_img.show()
+
+# img = apeal_data(x[0])
+# img = img.reshape(28, 28)
+# img_show(img)
+# y = predict(network, x[0])
+# print(y)
+# print(t[0])
+# exit()
+
+for i in range(0, len(x), batch_size):
+    x_batch = x[i:i+batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis=1)
+    accuracy_cnt += np.sum(p == t[i:i+batch_size])
+    #p = np.argmax(y) # 获取概率最高的元素的索引
+    # if p == t[i]:
+    #     accuracy_cnt += 1
+
+print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+
+def m(y, t):
+    a = y - t
+    b = a ** 2
+    s = np.sum(b)
+    x = 0.5 * s
+    return x
+
+def c(y, t):
+    delta = 1e-7
+    x = np.log(np.array([0.1,0.6,0.05]))
+    a = np.log(y + delta)
+    b = t * a
+    c = np.sum(b)
+    d = -c
+    return d
+
+t = [0,0,1,0,0,0,0,0,0,0]
+y = [0.1,0.05,0.6,0.0,0.05,0.1,0.0,0.1,0.0,0.0]
+a = c(np.array(y),np.array(t))
+print(a)
